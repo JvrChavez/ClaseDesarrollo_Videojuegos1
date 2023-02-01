a91bd886b5887fa390174ffbad3012e49fb989e1
JvrChavez <ariel.1999@outlook.com>
2023-02-01 14:01:47 -0700

    disque avance

--- Ventana2/MyThread.java
+++ Ventana2/MyThread.java
@@ -5,6 +5,7 @@ public class MyThread extends Thread {
     JLabel texto1;
     JButton btn;
     int n=1,i=180;
+    boolean ded=true;
     public void run(){
         btn.setEnabled(false);
         while(true){
@@ -12,12 +13,16 @@ public class MyThread extends Thread {
                 Thread.sleep(20);
                 
                 texto1.setText("Fuimonos");                
-                if (n>180) {
+                if (n>180 || n<0 || ded) {
                     texto1.setBounds(i,10,120,25);
                     i--;
                 } else{
+                    ded=false;
                     texto1.setBounds(n,10,120,25);
-                    n++;
+                    n--;
+                    if(n==0){
+                        ded=true;
+                    }
                 }
             } catch (Exception e) {
                 // TODO: handle exception
