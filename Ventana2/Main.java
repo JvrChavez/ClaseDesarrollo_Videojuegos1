f75056429f4bf77886cbbf982b26946b0a51b54b
jvrchavez <ariel.1999@outlook.com>
2023-02-07 16:56:07 -0700

    Bueno final final

--- Ventana2/Main.java
+++ Ventana2/Main.java
@@ -6,5 +6,7 @@ public class Main {
     public static void main(String[] args)throws IOException{
         new Ventana();
         //comentario prueba
+        //Esperemos que este si
+        //Ya listo listo
     }
 }
