25aa1f593d11e58c2b741cb950e104e0c7c6a833
jvrchavez <ariel.1999@outlook.com>
2023-02-14 03:13:53 -0700

    Madrugada 14 febrero 2023 ya corre link con el espacio

--- Ventana3/Imagen2.java
+++ Ventana3/Imagen2.java
@@ -6,7 +6,7 @@ public class Imagen2 extends JLabel implements Runnable,KeyListener{
     private String url1,url2;
     private ImageIcon icon;
     private int posX=10;
-    private boolean runStatus=false;
+    private boolean runStatus=false,correr=false,mover=false;
     public Imagen2(String url1,String url2){
         this.url1=url1;
         this.url2=url2;
@@ -15,25 +15,36 @@ public class Imagen2 extends JLabel implements Runnable,KeyListener{
     }
     public void run(){
         runStatus=true;
+        posX=10;
+        setBounds(posX,90,42,42);
     }//end run
     public void keyTyped(KeyEvent ke){}
     public void keyPressed(KeyEvent ke){
         if (runStatus) {
-            if (ke.getKeyCode()==KeyEvent.VK_RIGHT) {
-                posX++;
-                if ((posX%2)!=0) {
-                    icon=new ImageIcon(this.getClass().getResource(url2));
+            //Valores velocidad
+            if (ke.getKeyCode()==KeyEvent.VK_RIGHT) {        
+                if (correr) {
+                    posX+=3; 
                 }else{
-                    icon=new ImageIcon(this.getClass().getResource(url1));
-                }
-                if(ke.getKeyCode()==KeyEvent.VK_SHIFT){
-                    posX+=10;
-                    System.out.println("Entro");
-                }                
-                setBounds(posX,90,42,42);
-                setIcon(icon);
+                    posX++;
+                }                                                    
             }
+            if ((ke.getKeyCode()==KeyEvent.VK_SPACE)) {
+                correr=true; 
+            }
+            //Animacion movimiento
+            if ((posX%2)!=0) {
+                icon=new ImageIcon(this.getClass().getResource(url2));
+            }else{
+                icon=new ImageIcon(this.getClass().getResource(url1));
+            }
+            setBounds(posX,90,42,42);
+            setIcon(icon);
         }        
     }
-    public void keyReleased(KeyEvent ke){}
+    public void keyReleased(KeyEvent ke){
+        if ((ke.getKeyCode()==KeyEvent.VK_SPACE)) {
+            correr=false;
+        }
+    }
 }
