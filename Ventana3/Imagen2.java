131c25cfd97ffa822b4b83c0c47dedcc5cc50f43
jvrchavez <ariel.1999@outlook.com>
2023-02-14 12:30:40 -0700

    Clase 14 febrero 2023 ya brinca y corre  el link

--- Ventana3/Imagen2.java
+++ Ventana3/Imagen2.java
@@ -5,8 +5,8 @@ import java.awt.event.*;
 public class Imagen2 extends JLabel implements Runnable,KeyListener{
     private String url1,url2;
     private ImageIcon icon;
-    private int posX=10;
-    private boolean runStatus=false,correr=false,mover=false;
+    private int posX=10,posY=90;
+    private boolean runStatus=false,correr=false,mover=false,brincar=false;
     public Imagen2(String url1,String url2){
         this.url1=url1;
         this.url2=url2;
@@ -14,37 +14,63 @@ public class Imagen2 extends JLabel implements Runnable,KeyListener{
         setIcon(icon);
     }
     public void run(){
-        runStatus=true;
         posX=10;
-        setBounds(posX,90,42,42);
+        runStatus=true;
+        while(true){            
+            try {
+                Thread.sleep(50);
+                //Condicional brincar
+                if(brincar){                    
+                    posY=75;                    
+                }else{
+                    posY=90;
+                }
+                //Condicional mover y correr
+                if (correr && mover) {
+                    posX+=3; 
+                }else if(mover){
+                    posX++;
+                }
+                animacionMovimiento();                
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }            
+        }//end while             
     }//end run
+    //Valores movimiento
     public void keyTyped(KeyEvent ke){}
     public void keyPressed(KeyEvent ke){
-        if (runStatus) {
-            //Valores velocidad
-            if (ke.getKeyCode()==KeyEvent.VK_RIGHT) {        
-                if (correr) {
-                    posX+=3; 
-                }else{
-                    posX++;
-                }                                                    
+        if (runStatus) {            
+            if (ke.getKeyCode()==KeyEvent.VK_RIGHT) {  
+                mover=true;                                                                                      
             }
             if ((ke.getKeyCode()==KeyEvent.VK_SPACE)) {
-                correr=true; 
+                correr=true;                
             }
-            //Animacion movimiento
-            if ((posX%2)!=0) {
-                icon=new ImageIcon(this.getClass().getResource(url2));
-            }else{
-                icon=new ImageIcon(this.getClass().getResource(url1));
-            }
-            setBounds(posX,90,42,42);
-            setIcon(icon);
+            if ((ke.getKeyCode()==KeyEvent.VK_UP)) {
+                brincar=true;                
+            }        
         }        
     }
     public void keyReleased(KeyEvent ke){
-        if ((ke.getKeyCode()==KeyEvent.VK_SPACE)) {
+        if ((ke.getKeyCode()==KeyEvent.VK_SPACE)) {            
             correr=false;
         }
+        if ((ke.getKeyCode()==KeyEvent.VK_RIGHT)) {
+            mover=false;
+        }
+        if ((ke.getKeyCode()==KeyEvent.VK_UP)) {
+            brincar=false;
+        }
+    }
+    public void animacionMovimiento(){        
+        if ((posX%2)!=0) {
+            icon=new ImageIcon(this.getClass().getResource(url2));
+        }else{
+            icon=new ImageIcon(this.getClass().getResource(url1));
+        }
+        setBounds(posX,posY,42,42);
+        setIcon(icon);
     }
 }
