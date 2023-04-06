97f642263bbbb96790c0bbdad65507d128851563
JvrChavez <ariel.1999@outlook.com>
2023-04-06 06:34:39 -0700

    Avance madrugada 6 Abril 2023
    Se automatizo completamente a los bots, solo falta agregar pausa, musica y seleccionar un ganador, bueno tambien poner mas aleatorio los obstaculos

--- Caballos/Bots.java
+++ Caballos/Bots.java
@@ -2,13 +2,14 @@ package Caballos;
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.geom.*;
-public class Human1 extends JLabel implements Runnable ,KeyListener{
+public class Bots extends JLabel implements Runnable{
     private ImageIcon icon;
     private String url1,url2,url3,url4;
-    private boolean runStatus,moveStatus=false,upSatus=false,right=false,left=false,up=false,correr=false,falling=false;
+    private boolean runStatus,moveStatus=false,upSatus=false,right=true,left=false,up=false,correr=false,falling=false;
     JLabel roca,fondo,rocaother,rocaother2;
-    Bots botcito,botcito2;
-    public Human1(String url1,String url2,String url3,String url4){
+    Human1  player;
+    Bots botcito;
+    public Bots(String url1,String url2,String url3,String url4){
         this.url1=url1;
         this.url2=url2;
         this.url3=url3;
@@ -19,14 +20,18 @@ public class Human1 extends JLabel implements Runnable ,KeyListener{
     public void run(){
         runStatus=true;
         while(true){
-            if (right&&correr) {moveImage(5,50);}
-            else if(right){moveImage(3,70);}
+            if ((Math.random() > 0.5)) {correr=true;}//Condicion aleatoria de correr
+            if (right&&correr){moveImage(6,50);}
+            else if(right){moveImage(4,70);}
             else if(left){moveImage(-3,70);}
-            if(right&&correr&&up){salto(5,3,50);}
-            else if(right&&up){salto(3, 3, 50);}
+            if(roca.getX()-getX()<55 &&roca.getX()-getX()>45){up=((Math.random() > 0.7));}//Condicion aleatoria de se saltar
+            if(right&&correr&&up){salto(6,3,50);}
+            else if(right&&up){salto(4, 3, 50);}
             else if(up){salto(1, 3, 20);}
-            while(interseccion()){caer(2,70);}
-               
+            while(interseccion()){caer(3,70);}
+            //Se recomponen por default las cosas aleatorias
+            up=false;
+            correr=false;     
         }        
     }//end run
     public void caer(int power,int time){
@@ -60,20 +65,20 @@ public class Human1 extends JLabel implements Runnable ,KeyListener{
         if(getX()>640&&fondo.getX()>-1792&& !left){            
             fondo.setBounds(fondo.getX()-power,fondo.getY(),fondo.getWidth(),fondo.getHeight());
             roca.setBounds(roca.getX()-power,roca.getY(),roca.getWidth(),roca.getHeight());
-            botcito.setBounds(botcito.getX()-power,botcito.getY(),botcito.getWidth(),botcito.getHeight());
+            player.setBounds(player.getX()-power,player.getY(),player.getWidth(),player.getHeight());
             rocaother.setBounds(rocaother.getX()-power,rocaother.getY(),rocaother.getWidth(),rocaother.getHeight());
-            botcito2.setBounds(botcito2.getX()-power,botcito2.getY(),botcito2.getWidth(),botcito2.getHeight());
+            botcito.setBounds(botcito.getX()-power,botcito.getY(),botcito.getWidth(),botcito.getHeight());
             rocaother2.setBounds(rocaother2.getX()-power,rocaother2.getY(),rocaother2.getWidth(),rocaother2.getHeight());
             if(falling){
                 setBounds(getX(),getY(),60,36);
-            }else{setBounds(getX(),getY(),36,60);}            
+            }else{setBounds(getX(),getY(),36,60);}
         }else{              
             if(falling){
                 setBounds(getX()+power,getY(),60,36);
             }else{
                 setBounds(getX()+power,getY(),36,60);
             }
-        }     
+        }         
         try{Thread.sleep(time);}catch(InterruptedException e){e.printStackTrace();}                   
     }//end moveImage
     public void changeImage(){
@@ -90,21 +95,4 @@ public class Human1 extends JLabel implements Runnable ,KeyListener{
         }          
         setIcon(icon);
     }//end changeImage
-    public void keyTyped(KeyEvent ke){}
-    public void keyPressed(KeyEvent ke){
-        if (runStatus) {
-            if(ke.getKeyCode()==KeyEvent.VK_RIGHT){right=true;}
-            if(ke.getKeyCode()==KeyEvent.VK_LEFT){left=true;}
-            if(ke.getKeyCode()==KeyEvent.VK_SHIFT){correr=true;}
-            if(ke.getKeyCode()==KeyEvent.VK_UP){up=true;}
-        }
-    }//end keyPressed
-    public void keyReleased(KeyEvent ke){
-        if (runStatus) {
-            if(ke.getKeyCode()==KeyEvent.VK_RIGHT){right=false;}
-            if(ke.getKeyCode()==KeyEvent.VK_LEFT){left=false;}
-            if(ke.getKeyCode()==KeyEvent.VK_SHIFT){correr=false;}
-            if(ke.getKeyCode()==KeyEvent.VK_UP){up=false;}
-        }
-    }//end keyReleased
 }
--- Caballos/Ventana.java
+++ Caballos/Ventana.java
@@ -12,19 +12,48 @@ public class Ventana extends JFrame{
         background.setIcon(icon);
 
         Human1 man=new Human1("images/human1.png", "images/human2.png", "images/humanjump.png","images/humanfall.png");
+        Bots bot=new Bots("images/human1.png", "images/human2.png", "images/humanjump.png","images/humanfall.png");
+        Bots bot2=new Bots("images/human1.png", "images/human2.png", "images/humanjump.png","images/humanfall.png");
         JLabel roca=new JLabel();
+        JLabel rocabot=new JLabel();
+        JLabel rocabot2=new JLabel();
         ImageIcon rock=new ImageIcon(this.getClass().getResource("images/obstaculo.png"));
         roca.setIcon(rock);
+        rocabot.setIcon(rock);
+        rocabot2.setIcon(rock);
         man.roca=roca;
+        man.rocaother=rocabot;
+        man.rocaother2=rocabot2;
+        bot.roca=rocabot;
+        bot.rocaother=roca;
+        bot.rocaother2=rocabot2;
+        bot2.roca=rocabot2;
+        bot2.rocaother=roca;
+        bot2.rocaother2=rocabot;
+        man.fondo=background;
+        bot.fondo=background;
+        bot2.fondo=background;
+        man.botcito=bot;
+        man.botcito2=bot2;
+        bot.player=man;
+        bot.botcito=bot2;
+        bot2.player=man;
+        bot2.botcito=bot;
         JButton btnStart=new JButton("Start");
 
         //Focus
         man.setFocusable(true);
+        bot.setFocusable(false);
+        bot2.setFocusable(false);
         btnStart.setFocusable(false);
 
         //Bounds
-        man.setBounds(30,155,36,60);
-        roca.setBounds(200,190,30,30);
+        man.setBounds(30,500,36,60);
+        bot.setBounds(30,600,36,60);
+        bot2.setBounds(30,400,36,60);
+        roca.setBounds(200,530,30,30);
+        rocabot.setBounds(200,630,30,30);
+        rocabot2.setBounds(200,430,30,30);
         btnStart.setBounds(1100,30,70,25);
         background.setBounds(0,-157,3840,1080);
         //Listeners
@@ -33,6 +62,10 @@ public class Ventana extends JFrame{
                 if (ae.getSource()==btnStart) {
                     Thread t=new Thread(man);
                     t.start();
+                    Thread t2=new Thread(bot);
+                    t2.start();
+                    Thread t3=new Thread(bot2);
+                    t3.start();
                 }
             }
         };
@@ -40,7 +73,11 @@ public class Ventana extends JFrame{
         man.addKeyListener(man);
         //Adds
         add(man);
+        add(bot);
+        add(bot2);
         add(roca);
+        add(rocabot);
+        add(rocabot2);
         add(btnStart);
         add(background);
         
